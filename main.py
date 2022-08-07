549a6efa620886d5228c59f126098dfedcda93fd
UFifty50 <UFifty50.github@gmail.com>
2022-08-07 18:19:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,64 @@
 import os
 from python_aternos import Client
+from python_aternos.aterrors import ServerStartError
 import discord
-from discord.ext import commands
+from discord import Message
+from discord.ext import commands, tasks
+from discord.ext.commands import Context
+from dotenv import load_dotenv
 
 intents = discord.Intents.default()
-#intents.message_content = True
 bot = commands.Bot(command_prefix='>', intents=intents)
 
-#aternos = Client.from_hashed(os.environ['uname'], os.environ['pswd'])
-"""
-servs = aternos.list_servers()
-serv = None
-for testserv in servs:
-    if serv.address == 'eggggggy.aternos.me':
-        serv = testserv
- """
-       
-@bot.command()
-async def start(ctx):
-    await ctx.send('Starting server...')
- #   serv.start()
-    os.system("python login.py")
-    await ctx.send('Server `eggggggy.aternos.me` started!')
+load_dotenv()
 
-bot.run(os.environ['token'])
+aternos = Client.from_credentials(os.getenv('uname'), os.getenv('pswd'))
+
+def getServer():
+    servs = aternos.list_servers()
+    serv = None
+    for testserv in servs:
+        if testserv.address == 'eggggggy.aternos.me':
+            serv = testserv
+
+starting = False
+
+@bot.event
+async def on_ready():
+        print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+        print('------')
+        await bot.change_presence(activity=discord.Game(name='>help for help'))
+        getServer()
+
+@tasks.loop(seconds=1)
+async def bgTask(ctx: Context, msg: Message):
+  global serv
+  if serv.status != 'online':
+    print(serv.status)
+    print(serv.status_num)
+    aternos.refresh_servers([serv.servid])
+    getServer()
+    global wait
+    wait += 1
+    await msg.edit(content=msg.content[0:18] + f" `{wait}`")
+  else:
+    await ctx.send(f'Server `eggggggy.aternos.me` started in `{wait}` seconds')
+    global starting
+    starting = False
+    bgTask.stop()
+
+@bot.command()
+async def start(ctx: Context):
+  global starting
+  if starting:
+    await ctx.send(f"Server `{serv.address}` is already starting")
+  else:
+    try:
+      serv.start(True, True)
+      starting = True
+      startingMsg = await ctx.send('Starting server...')
+      bgTask.start(ctx, startingMsg)
+    except ServerStartError:
+      await ctx.send(f'Server `{serv.address}` is already running')
+
+bot.run(os.getenv('token'))
